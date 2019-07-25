0186959fd2b39d525815b9a7c9e785c3100fce2e
joao_marcolan <jamarcolan@gmail.com>
2019-07-25 14:37:09 -0300

    A calsse Arrumando_entrada passou nos testes unitários e está com a lógica funcionando. Porém é necessário refatorações no código e deixar mais claro a api de uso da classe.

--- conversor_dados.py
+++ conversor_dados.py
@@ -61,10 +61,7 @@ class Converte_numero:
 class Arrumando_entrada():
     def __init__(self,vetor_dados):
         self.vetor_dados = vetor_dados
-
-        self.pos_critica = self._encontrando_posicao_critica()
-
-        self.vetor_dados_saida = []
+        # self.vetor_dados_saida = self._arrumando_entrada()
 
     
     def _inverte_a_lista(self):
@@ -76,7 +73,7 @@ class Arrumando_entrada():
     def _encontrando_posicao_critica(self):
         self.list_invertida = self._inverte_a_lista()
 
-        self.list_invertida = list(reversed(self.vetor_dados))
+        # self.list_invertida = list(reversed(self.vetor_dados))
         pos_critica = []
         regra_tamanho_3 = len(self.list_invertida) >= 2
         regra_tamanho_5 = len(self.list_invertida) >= 5
@@ -89,7 +86,49 @@ class Arrumando_entrada():
 
         return pos_critica
 
+
+
+
+    # def _valor_excecao(self):
+    #     concertando_excecao = []
+    #     for i,excecao in enumerate(self.excecoes):
+    #         if excecao:
+    #             concertando_excecao.append(self.list_invertida[self.pos_critica[i-1]] + self.list_invertida[self.pos_critica[i-1]])
+    #     return concertando_excecao
+
+    def _teste_posicao_critic(self,i):
+        for cont,critica in enumerate(self.pos_critica):
+                if i == critica:
+                    return self._testando_se_aconteceu_exececao(cont)
+        return False
+
+    def _testando_se_aconteceu_exececao(self,cont):
+        return self.excecoes[cont]
+
+
+
+    def concerta_vetor(self,saida,valor,i):
+        saida.pop(len(saida)-1) #tirando o ultimo valor da lista
+        valor_1 = self.list_invertida[i] + self.list_invertida[i-1]
+        saida.append(valor_1)
+
+
+    def _arrumando_entrada(self):
+        self.pos_critica = self._encontrando_posicao_critica()
+        self.excecoes = self._testando_regra_excecao()
+        
+
+        saida = []
+        for i,valor in enumerate(self.list_invertida):
+            if self._teste_posicao_critic(i):
+                self.concerta_vetor(saida,valor,i)
+            else:
+                saida.append(valor)
+
+        return list(reversed(saida))
+
     def _testando_regra_excecao(self):
+        self.pos_critica = self._encontrando_posicao_critica()
 
         resul_regra = []
         for pos_critica in self.pos_critica:
@@ -112,3 +151,15 @@ class Arrumando_entrada():
 
 
 
+# bloco_teste = [[200, 20, 2],
+#                     [10,9],
+#                     [20,9],
+#                     [10000,9000, 300,20,1],
+#                     [20000,9000, 300,20,1],
+#                     [10000,9000, 300,10,9],
+#                     [1],
+#                     [20000,9000, 300,10,9]]
+
+# for count,teste in enumerate(bloco_teste):
+#     parser_entrada = Arrumando_entrada(teste)
+#     saida = parser_entrada._arrumando_entrada()
--- test_conversor_dados.py
+++ test_conversor_dados.py
@@ -1,10 +1,5 @@
 from conversor_dados import *
 
-# def test_answer():
-#     assert inc(3) == 4
-
-
-
 
 def test_inverte_lista():
     bloco_teste = [[200, 20, 2],
@@ -88,3 +83,28 @@ def test_regra_excecao():
 #                     [20000,9000, 300,20,1],
 #                     [19000, 300,19]]
     
+
+def test_saida_arrumada():
+
+    bloco_teste = [[200, 20, 2],
+                    [10,9],
+                    [20,9],
+                    [10000,9000, 300,20,1],
+                    [20000,9000, 300,20,1],
+                    [10000,9000, 300,10,9],
+                    [1],
+                    [20000,9000, 300,10,9]]
+    
+    bloco_teste_saida =[[200, 20, 2],
+                    [19],
+                    [20,9],
+                    [19000, 300,20,1],
+                    [20000,9000, 300,20,1],
+                    [19000, 300,19],
+                    [1],
+                    [20000,9000, 300,19]]
+
+    for count,teste in enumerate(bloco_teste):
+        parser_entrada = Arrumando_entrada(teste)
+        saida = parser_entrada._arrumando_entrada()
+        assert saida == bloco_teste_saida[count]
