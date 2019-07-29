77582ec9c9aa1376804021e6314956bbd5de0587
joao_marcolan <jamarcolan@gmail.com>
2019-07-29 14:55:48 -0300

    Documentando o Servidor.

--- servidor.py
+++ servidor.py
@@ -1,3 +1,19 @@
+""" Módulo responsável por criar o servidor HTTTP.
+Example:
+    Para rodar o módulo, tenha instalado o Python 3.5, abra um termina direciona a essa parte e digite:
+    literal blocks::
+        $ python -m pip install -r requirements.txt
+    
+    Após realizar a instalação das dependencias do projeto digite:
+    literal blocks::
+        $ python servidor.py
+
+Todo:
+    * Ver notas dentro das classes.
+
+"""
+
+
 from sistema import Transforma_numero_extensao
 
 from flask import Flask
@@ -8,15 +24,24 @@ import os
 app = Flask(__name__)
 
 class Saida_Desafio_Certi:
+    """Adequa o sistema para a saída experada pelo pessoal da CERTI.
+    """
     def __init__(self,string):
+        """ Método inicializador
+        Args:
+            string (str): Uma string que contenha um numero para ser convertido para sua forma por extensa. 
+        """
         self.transformador = Transforma_numero_extensao(string)
         self.texto_extenso = self.transformador .get_saida()  
 
     def saida(self):
+        """Pega a saida adequa e experada pelo pessoal da CERTI.
+        """
         return json.dumps({"extenso" :self.texto_extenso})
         
         # return jsonify(extenso=self.texto_extenso)
 
+# Como é uma API seria interessante na URN ter o versionamento (e.g., /v1/<string_entrada>)
 @app.route("/<string_entrada>")
 def hello1(string_entrada):
     saida_extenso  = Saida_Desafio_Certi(string_entrada)
